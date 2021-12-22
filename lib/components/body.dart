595a1ac08f1dd85ab64ea66246d8f4a8c7e05ceb
tadakun8 <tadakun8.lab@gmail.com>
2021-12-22 18:16:24 +0900

    feat: Add display messages

--- lib/components/body.dart
+++ lib/components/body.dart
@@ -11,11 +11,55 @@ class Body extends StatelessWidget {
         fit: StackFit.expand,
         children: [
           FractionallySizedBox(
-            alignment: Alignment.center,
+            alignment: Alignment.centerRight,
             widthFactor: 0.6,
             child: Image.asset(
               'lib/assets/image_01.png',
               scale: 0.85,
+            ),
+          ),
+          FractionallySizedBox(
+            alignment: Alignment.centerLeft,
+            widthFactor: 0.6,
+            child: Padding(
+              padding: const EdgeInsets.only(left: 48),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    'Hello!',
+                    style: TextStyle(
+                        fontSize: 60,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: "Montserrat-Regular",
+                        color: Color(0xFF8591B0)),
+                  ),
+                  RichText(
+                    text: const TextSpan(
+                      text: 'Welcom To ',
+                      style: TextStyle(
+                        fontSize: 60,
+                        color: Color(0xFF8591B0),
+                      ),
+                      children: [
+                        TextSpan(
+                          text: "Britu",
+                          style: TextStyle(
+                            fontSize: 60,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black87,
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.only(left: 12.0, top: 20),
+                    child: Text("LET’S EXPLORE THE WORLD"),
+                  ),
+                ],
+              ),
             ),
           )
         ],
